f6e428b3b232402b7727d17026cf7cd2a59b3b70
Brad Masciotra <63364775+bradmasy@users.noreply.github.com>
2022-05-09 13:13:46 -0700

    ain

---
+++ public/scripts/signup.js
@@ -0,0 +1,112 @@
+/**
+ * signup.JS for the signup page
+ */
+
+/**
+ * Constant Variables
+ */
+
+const $backButton        = $("#back-button");
+const $confirmButton     = $("#confirm-button");
+const $confirmPassword   = $("#password2");
+const $userPassword      = $("#password1");
+const $showPasswordBox   = $("#show");
+const $showPasswordTitle = $("#pass-title");
+
+
+
+/**
+ * Let Variables
+ */
+
+let passwordVisible = false; // for revealing the password.
+
+/**
+ * Functions
+ */
+
+/**
+ * Event Handlers
+ */
+
+$backButton.on("click", () => {
+  window.location.href = "../index.html";
+});
+
+
+
+/**
+ * Turns function handlers on for show password.
+ */
+function showPassword()
+{
+    $showPasswordBox.on("click",() => {
+
+        if(!passwordVisible)
+        {
+            $showPasswordTitle.html("Hide Password");
+            $userPassword.attr("type","text");
+            $confirmPassword.attr("type","text");
+            passwordVisible = true;
+        }
+        else
+        {
+            $showPasswordTitle.html("Show Password");
+            $userPassword.attr("type","password");
+            $confirmPassword.attr("type","password");
+            passwordVisible = false;
+        }
+    })
+}
+
+function proceedToHome() {
+  console.log("successful Signup");
+  window.location.href = "/success.html";
+}
+
+function checkUserExists(data) {
+  if (data.length === 0) {
+    console.log("User not found!");
+    alert("User not found");
+  } else {
+    proceedToHome();
+  }
+}
+
+function attemptSignup() {
+  console.log("attemptSignup" + "got called!");
+  console.log($("#username").val());
+  console.log($("#email").val());
+  console.log($("#password1").val());
+  console.log($("#password2").val());
+
+  adminIsChecked = false;
+  if ($("#admin-status").is(":checked")) adminIsChecked = true;
+
+
+  if ($("#password1").val() === $("#password2").val()) {
+    $.ajax({
+      url: "http://localhost:5000/attemptSignup",
+      //   url: "https://radiant-anchorage-93970.herokuapp.com/findUnicornByWeight",
+      type: "POST",
+      data: {
+        username: $("#username").val(),
+        email: $("#email").val(),
+        password: $("#password1").val(),
+        admin: adminIsChecked
+      },
+      success: checkUserExists,
+    });
+    // resetPage();
+  } else {
+    alert("passwords do not match");
+  }
+}
+
+function setup() {
+  console.log("document ready");
+  showPassword();
+  $confirmButton.on("click", attemptSignup);
+}
+
+$(document).ready(setup);
---
+++ server.js
@@ -0,0 +1,120 @@
+// homepage
+// executed first when the serve is initiated
+
+const express    = require("express");
+const app        = express();
+const https      = require("https");
+const bodyparser = require("body-parser");
+const mongoose   = require("mongoose");
+const session    = require("express-session");
+const { use }    = require("express/lib/application");
+const res        = require("express/lib/response");
+
+app.set("view engine", "ejs");
+
+app.listen(process.env.PORT || 5000, function (err) {
+  if (err) console.log(err);
+});
+
+app.use(
+  bodyparser.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(session({secret:"shhhh", saveUninitialized:true, resave:true}));
+
+mongoose.connect(
+  "mongodb+srv://fuel_line_2022:fuel@cluster0.vcuj9.mongodb.net/FuelLineDTC12?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+//  mongoose.connect("mongodb://localhost:27017/Fuel_Line"); // our database on local host, not yet on server...
+
+  const userSchema = new mongoose.Schema({
+    username: String,
+    passwordt: String,
+  });
+  const userModel = mongoose.model("users", userSchema);
+ 
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+app.get("/login", function (req, res) {
+  res.sendFile(__dirname + "/public/login.html");
+});
+
+function checkUserExists(data) {
+ 
+  let valid = true;
+
+  if (data.length === 0) {
+    valid = false;
+  } 
+
+  return valid;
+}
+
+function initiateSession(req,users)
+{
+  if(checkUserExists(users)){
+    req.session.authenticated = true; // user gets authenticated.
+    req.session.user          = users; 
+    console.log(`welcome ${users[0].username}`);
+  }
+  else
+  {
+    req.session.authenticated = false;
+    console.log(`invalid user`);
+  }
+}
+
+app.use(express.static("./public"));
+
+app.post("/attemptLogin", function (req, res) {
+  console.log("req. has been received");
+  
+  userModel.find(
+    {
+      $and: [{ username: req.body.username }, { password: req.body.password }],
+    },
+    function (err, users) {
+      if (err) 
+      {
+        console.log("Error " + err);
+        req.session.authenticated = false; // user gets authenticated.
+        console.log("FAIL");
+      } 
+      else 
+      {
+        initiateSession(req, users);    
+      }
+    }
+  );
+});
+
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+/**
+ * Database Connection
+ */
+
+ app.post("/displayUsersToAdmin", function (req, res) {
+  console.log("req. has been recieved");
+  console.log(req.body.username);
+
+  userModel.find({ name: req.body.username }, function (err, users) {
+    if (err) {
+      console.log("Error " + err);
+    } else {
+      console.log("Data " + users);
+    }
+    res.send(users);
+  });
+});
+
+console.log("Server Running");
+
